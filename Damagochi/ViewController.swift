ea47679afa0748291d0ff791621b6847da5ecd98
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-06-06 21:07:29 +0900

    Initial Commit

---
+++ Damagochi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damagochi
+//
+//  Created by 최승범 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
